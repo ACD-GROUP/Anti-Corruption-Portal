da9227100853f93c9011debe5613413e6c4d0863
Shukhrat Rustamov <shukhratrustamov@Shukhrats-iMac.local>
2024-01-26 20:53:23 -0500

    FEATURE: Initialized project and done all documentation.

---
+++ src/app/layout.tsx
@@ -0,0 +1,13 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+};
+
+export default function RootLayout({ children, }: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
